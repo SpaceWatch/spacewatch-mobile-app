fbb20f9c4d6f4f29637272da814adda78cf3a6c6
zameschua <zames.chua@gmail.com>
2021-05-04 23:57:28 +0800

    Implement models

--- src/types/models.ts
+++ src/types/models.ts
@@ -1,39 +1,56 @@
-export type BlueprintKey = string;
+/**
+ * Move asteroid protocol specific code to asteroid.js
+ */
 
-export interface AsteroidBlueprint {
-  blueprintKey: BlueprintKey; // Formatted as `blockchain.protocol.blueprint`
+/**
+ * Please sort this in alphabatical order so that others can find duplicates if any exist
+ */
+export enum WatcherKey {
+  TERRA__ANCHOR__LTV_RATIO = "TERRA__ANCHOR__IS_HIGH_LTV"
+};
+
+export type WatcherFieldKey = string;
+
+export interface WatcherConfig {
+  key: WatcherKey; // Formatted as `blockchain.protocol.method`
+  blockchain: string; // eg. `terra`
+  protocol: string; // eg. `anchor`, use keyword `base` for base layer queries
+  method: string; // eg. `isHighLtv`
+
+  // Alert me when: Anchor LTV is too high
   name: string;
+  // eg. Send an alert when my Anchor LTV is above x%
   description: string;
-  fields: BlueprintField[]
+
+  fields: WatcherField[];
 }
 
 // We store all fields as strings, even integers and floats
-export interface BlueprintField {
-  key: FieldKey; // Should be unique among all the fields
+export interface WatcherField {
+  key: WatcherFieldKey; // Should be unique among other fields belonging to the same `blockchain.protocol.method` watcher
   name: string;
   description: string;
   validationRegex: string; // Regex string
 }
 
 
-export type FieldKey = string;
-export type TemplateString = string;
-
-
-export interface AsteroidWatcher {
-  walletAddress: string;
-  blueprint: AsteroidBlueprint;
-  fieldValuesByKey: Record<FieldKey, FieldValue>;
+export interface WatcherSubscription {
+  // watcherAddress: string;
+  watcherConfig: WatcherConfig;
+  fieldValuesByKey: Record<WatcherFieldKey, FieldValue>;
 }
 
-export interface FieldValue {
-  key: FieldKey;
-  value: TemplateString;
+export interface FieldValue extends WatcherField {
+  key: WatcherFieldKey;
+  name: string;
+  description: string;
+  validationRegex: string; // Regex string
+  value: string; // We store all values as strings, even integers and floats
 }
 
 // Configuration for the alert message to show to the user
 export interface NotificationConfig {
   title: string;
   body: string;
-  data?: any; // Pass arbitrary data to the app? Not sure if we want this yet
+  // data?: any; // Pass arbitrary data to the app? Not sure if we want this yet
 }

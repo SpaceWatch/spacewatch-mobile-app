ffc56f0920c4ca5f453a41a24dfb0743fa44edd7
zameschua <zames.chua@gmail.com>
2021-05-07 02:16:45 +0800

    Implement Asteroid

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -4,6 +4,8 @@ import { scheduleNotification } from "../common/pushNotifications";
 import { sendLuna } from "../common/testApi";
 import { useHistory } from "react-router-native";
 import Routes from "../routes";
+import { LCDClient, MnemonicKey } from "@terra-money/terra.js";
+import Asteroid from "../common/Asteroid";
 
 const Home = () => {
   const history = useHistory();
@@ -19,6 +21,22 @@ const Home = () => {
         title="HELP"
         onPress={async () => {
           await scheduleNotification();
+        }}
+      />
+      <Button
+        title="Call the blokchain"
+        onPress={async () => {
+          const tequilaConfig = {
+            URL: 'https://tequila-lcd.terra.dev',
+            chainID: 'tequila-0004',
+          }
+          const terra = new LCDClient(tequilaConfig);
+          const mk = new MnemonicKey({mnemonic: 'balance attitude unveil maximum february sunny fresh learn minute arrange song media squirrel sail judge dance immune core asset sample escape talent donate hood'})
+          const wallet = terra.wallet(mk);
+
+          const asteroid = Asteroid(wallet);
+          console.log(await asteroid.getAlerts());
+          console.log(await asteroid.getSubscriptions());
         }}
       />
       <Button
